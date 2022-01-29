6b1e3fc8f8310a51b45415f09ea1123cb4bec857
ahmedA1987 <92261674+ahmedA1987@users.noreply.github.com>
2022-01-29 22:32:11 +0200

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -11,7 +11,7 @@ the signal
 from math import *
 
 
-def control_signal(linear_velocity, angular_velocity, f1=0.6, f2=1, max_turn=42):
+def control_signal(linear_velocity, angular_velocity, f1=0.6, f2=0.6, max_turn=42):
     """
     passed parameters
         linear_velocity: type float:
@@ -40,7 +40,6 @@ def control_signal(linear_velocity, angular_velocity, f1=0.6, f2=1, max_turn=42)
     alpha_1 = acos((pow(f1, 2) + pow(f3, 2) - pow(radius, 2)) / (2 * f1 * f3))
     alpha_2 = acos((pow(f2, 2) + pow(f3, 2) - pow(radius, 2)) / (2 * f2 * f3))
     theata = 180 - degrees(alpha_1) - degrees(alpha_2)
-
     if theata >= max_turn:
         return direction
     else:
